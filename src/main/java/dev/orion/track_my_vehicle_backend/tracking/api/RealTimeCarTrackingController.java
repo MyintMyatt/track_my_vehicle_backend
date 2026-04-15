b5960c1d600a620353b5a87eeb489754d1b5cf44
orion <nnmm5122004@gmail.com>
2026-04-15 18:07:07 +0630

    stomp config and test receive data

---
+++ src/main/java/dev/orion/track_my_vehicle_backend/tracking/api/RealTimeCarTrackingController.java
@@ -0,0 +1,31 @@
+package dev.orion.track_my_vehicle_backend.tracking.api;
+
+import dev.orion.track_my_vehicle_backend.tracking.dto.request.LocationUpdate;
+import dev.orion.track_my_vehicle_backend.tracking.service.LocationUpdateBroadcastService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class RealTimeCarTrackingController {
+
+    private final LocationUpdateBroadcastService broadcastService;
+
+    @MessageMapping("/track")
+    public void handleLiveTrack(@Payload LocationUpdate update){
+        log.info("Received WebSocket update: {}", update);
+        broadcastService.broadcast(update);
+    }
+
+    // for tracklet
+    @PostMapping("/api/update/locations")
+    public void receiveLocation(@RequestBody LocationUpdate update){
+        broadcastService.broadcast(update);
+    }
+}
