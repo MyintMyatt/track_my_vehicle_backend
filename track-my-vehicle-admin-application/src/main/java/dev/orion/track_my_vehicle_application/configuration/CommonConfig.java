7b88f312059fb568ec61f02a71720538d5c6f235
orion <nnmm5122004@gmail.com>
2026-05-19 23:30:29 +0630

    remove config

--- track-my-vehicle-admin-application/src/main/java/dev/orion/track_my_vehicle_application/configuration/CommonConfig.java
+++ track-my-vehicle-admin-application/src/main/java/dev/orion/track_my_vehicle_application/configuration/CommonConfig.java
@@ -12,23 +12,23 @@ import org.springframework.web.multipart.support.StandardServletMultipartResolve
 @ComponentScan(basePackages = {"dev.orion.commons", "dev.orion.track_my_vehicle_domain"})
 public class CommonConfig {
 
-    @Bean
-    public StandardServletMultipartResolver multipartResolver() {
-        StandardServletMultipartResolver resolver = new StandardServletMultipartResolver();
-        // This forces Spring to strictly respect the Servlet container (Tomcat) settings
-        resolver.setStrictServletCompliance(true);
-        return resolver;
-    }
-
-    @Bean
-    public MultipartConfigElement multipartConfigElement() {
-        MultipartConfigFactory factory = new MultipartConfigFactory();
-
-        // Explicitly set sizes using DataSize parsing utility
-        factory.setMaxFileSize(DataSize.ofMegabytes(200));
-        factory.setMaxRequestSize(DataSize.ofMegabytes(500));
-
-        return factory.createMultipartConfig();
-    }
+//    @Bean
+//    public StandardServletMultipartResolver multipartResolver() {
+//        StandardServletMultipartResolver resolver = new StandardServletMultipartResolver();
+//        // This forces Spring to strictly respect the Servlet container (Tomcat) settings
+//        resolver.setStrictServletCompliance(true);
+//        return resolver;
+//    }
+//
+//    @Bean
+//    public MultipartConfigElement multipartConfigElement() {
+//        MultipartConfigFactory factory = new MultipartConfigFactory();
+//
+//        // Explicitly set sizes using DataSize parsing utility
+//        factory.setMaxFileSize(DataSize.ofMegabytes(200));
+//        factory.setMaxRequestSize(DataSize.ofMegabytes(500));
+//
+//        return factory.createMultipartConfig();
+//    }
 
 }
